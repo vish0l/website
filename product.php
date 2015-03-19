47150970fa6070a000b6251d29f87aee4fadf2cf
vishal singh <join4m.com@gmail.com>
2015-03-19 12:56:02 +0530

    working without filters

--- product.php
+++ product.php
@@ -2,9 +2,29 @@
 error_reporting(1);
 ini_set('display_errors', 'On');
 
-$conn = mysqli_connect("localhost", "root", "Newhackold57", "product_data");
+$conn = mysqli_connect("localhost", "pse", "pse", "product_data");
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 $name=$_GET['name'];
+
+$fsql = "SELECT * FROM flipkart_mobile where product_name like '%".$name."%'";
+$ssql = "SELECT * FROM snapdeal_mobile where product_name like '%".$name."%'";
+$asql = "SELECT * FROM amazon_mobile where product_name like '%".$name."%'";
+
+$fresult = mysqli_query($conn, $fsql);
+$sresult = mysqli_query($conn, $ssql);
+$aresult = mysqli_query($conn, $asql);
+$fresult=mysqli_fetch_assoc($fresult);
+$sresult=mysqli_fetch_assoc($sresult);
+$aresult=mysqli_fetch_assoc($aresult);
+
+echo "<table border='1'>";
+echo "<tr><td>Product Name</td><td>product price</td><td>product link</td></tr><br>";
+echo "<tr><td>".$fresult[product_name]." ".$fresult[product_tag]."</td><td>".$fresult[product_price]."</td><td><a href='".$fresult[product_url]."'>Flipkart.com</a></td></tr><br>";
+echo "<tr><td>".$sresult[product_name]."</td><td>".$sresult[product_price]."</td><td><a href='".$sresult[product_url]."'>Snapdeal.com</a></td></tr><br>";
+echo "<tr><td>".$aresult[product_name]."</td><td>".$aresult[product_price]."</td><td><a href='".$aresult[product_url]."'>Amazon.in</a></td></tr><br>";
+echo "</table>";
+echo $fresult[product_specs];
+?>
