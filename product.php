ffc781adaf9f9f97f44b6c967e9c077e47393fd2
vishal singh <join4m.com@gmail.com>
2015-04-16 10:52:08 -0400

    added bootstrap in product.php

--- product.php
+++ product.php
@@ -19,17 +19,35 @@ $aresult = mysqli_query($conn, $asql);
 $fresult=mysqli_fetch_assoc($fresult);
 $sresult=mysqli_fetch_assoc($sresult);
 $aresult=mysqli_fetch_assoc($aresult);
-
-echo "<table border='1' cellpadding='0' cellspacing='0' width='900px' style='border-collapse:collapse;''>";
-echo "<tr><td><b>Product Name</b></td><td><b>Product Price</b></td><td><b>Product Link</b></td></tr><br>";
-echo "<tr><td>".$fresult[product_name]." ".$fresult[product_tag]."</td><td>".$fresult[product_price]."</td><td><a href='".$fresult[product_url]."' target='_blank'>Flipkart.com</a></td></tr><br>";
-echo "<tr><td>".$sresult[product_name]."</td><td>Rs. ".substr($sresult[product_price],0,-5)."</td><td><a href='".$sresult[product_url]."' target='_blank'>Snapdeal.com</a></td></tr><br>";
-echo "<tr><td>".$aresult[product_name]."</td><td>Rs. ".$aresult[product_price]."</td><td><a href='".$aresult[product_url]."' target='_blank'>Amazon.in</a></td></tr><br>";
-echo "</table>";
-echo $fresult[product_specs];
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<link rel="stylesheet" type="text/css" href="css1.css">
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="css1.css">
 </head>
+<body>
+<div class="container">
+  <h2><?php echo $fresult[product_name]." ".$fresult[product_tag]; ?></h2>
+  <p>Online Price Of <?php echo $fresult[product_name]." ".$fresult[product_tag]; ?>:</p>            
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>Product Name</th>
+        <th>Product Price</th>
+        <th>Product URL</th>
+      </tr>
+    </thead>
+    <tbody>
+<tr><td><?php echo $fresult[product_name]." ".$fresult[product_tag]; ?></td><td><?php echo $fresult[product_price]; ?> </td><td><a href="<?php echo $fresult[product_url]; ?>" target='_blank'>Flipkart.com</a></td></tr>
+<tr><td><?php echo $sresult[product_name]; ?></td><td>Rs. <?php echo substr($sresult[product_price],0,-5); ?></td><td><a href="<?php echo $sresult[product_url]; ?>" target='_blank'>Snapdeal.com</a></td></tr>
+<tr><td><?php echo $aresult[product_name]; ?></td><td>Rs. <?php echo $aresult[product_price]; ?></td><td><a href="<?php echo $aresult[product_url]; ?>" target='_blank'>Amazon.in</a></td></tr>
+</tbody></table></div>
+<?php echo $fresult[product_specs]; ?>
+</body>
 </html>
